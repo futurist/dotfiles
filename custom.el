6a8537a5df785f02c828ea629fb5751ae1130130
1111hui <26634873@qq.com>
2016-04-26 17:05:54 +0800

    ADD: remark for the using of search-whitespace-regexp M-s SPC toggle

--- custom.el
+++ custom.el
@@ -349,7 +349,8 @@ Including indent-buffer, which should not be called automatically on save."
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(ac-use-fuzzy nil)
- '(search-whitespace-regexp ".*?")      ;space seperated multi key word
+ ;; using M-s SPC to toggle loose/lax
+ '(search-whitespace-regexp ".*?")      ;space seperated multi key word instead of "\\s-+"
  '(avy-timeout-seconds 0.4)
  '(avy-keys '(?a ?b ?c ?d ?e ?f ?g ?h ?i ?j ?k ?l ?m ?n ?o ?p ?q ?r ?s ?t ?u ?v ?w ?x ?y))
  ;; '(avy-keys '(?0 ?1 ?2 ?3 ?4 ?5 ?6 ?7 ?8 ?9))
