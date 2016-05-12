edfeacfc33e9757429730501edac46985050326b
1111hui <26634873@qq.com>
2016-05-12 20:22:44 +0800

    ADD: org-mode export to md from org file

--- custom.el
+++ custom.el
@@ -139,6 +139,117 @@ Version 2015-06-12"
 (setq org-reveal-root (concat "file:///" (expand-file-name "~/download/reveal.js")))
 (setq org-startup-with-inline-images nil)
 
+(require-package 'ox-gfm)
+
+(defvar hugo-content-dir "~/Hugo/Sites/12345.menu/content/"
+  "Path to Hugo's content directory")
+
+
+;; Hugo convert org to md
+;; http://www.holgerschurig.de/en/emacs-blog-from-org-to-hugo/
+(defun hugo-ensure-property (property)
+  "Make sure that a property exists. If not, it will be created.
+
+Returns the property name if the property has been created,
+otherwise nil."
+  (if (org-entry-get nil property)
+      nil
+    (progn (org-entry-put nil property "")
+           property)))
+
+(defun hugo-ensure-properties ()
+  "This ensures that several properties exists. If not, these
+properties will be created in an empty form. In this case, the
+drawer will also be opened and the cursor will be positioned
+at the first element that needs to be filled.
+
+Returns list of properties that still must be filled in"
+  (require 'dash)
+  (let ((current-time (format-time-string (org-time-stamp-format t t) (org-current-time)))
+        first)
+    (save-excursion
+      (unless (org-entry-get nil "TITLE")
+        (org-entry-put nil "TITLE" (nth 4 (org-heading-components))))
+      (setq first (--first it (mapcar #'hugo-ensure-property '("HUGO_TAGS" "HUGO_TOPICS" "HUGO_FILE"))))
+      (unless (org-entry-get nil "HUGO_DATE")
+        (org-entry-put nil "HUGO_DATE" current-time)))
+    (when first
+      (goto-char (org-entry-beginning-position))
+      ;; The following opens the drawer
+      (forward-line 1)
+      (beginning-of-line 1)
+      (when (looking-at org-drawer-regexp)
+        (org-flag-drawer nil))
+      ;; And now move to the drawer property
+      (search-forward (concat ":" first ":"))
+      (end-of-line))
+    first))
+
+(defun hugo ()
+  (interactive)
+  (unless (hugo-ensure-properties)
+    (let* ((title    (concat "title = \"" (org-entry-get nil "TITLE") "\"\n"))
+           (date     (concat "date = \"" (format-time-string "%Y-%m-%d" (apply 'encode-time (org-parse-time-string (org-entry-get nil "HUGO_DATE"))) t) "\"\n"))
+           (topics   (concat "topics = [ \"" (mapconcat 'identity (split-string (org-entry-get nil "HUGO_TOPICS") "\\( *, *\\)" t) "\", \"") "\" ]\n"))
+           (tags     (concat "tags = [ \"" (mapconcat 'identity (split-string (org-entry-get nil "HUGO_TAGS") "\\( *, *\\)" t) "\", \"") "\" ]\n"))
+           (fm (concat "+++\n"
+                       title
+                       date
+                       tags
+                       topics
+                       "+++\n\n"))
+           (file     (org-entry-get nil "HUGO_FILE"))
+           (coding-system-for-write buffer-file-coding-system)
+           (backend  'md)
+           (blog))
+      ;; try to load org-mode/contrib/lisp/ox-gfm.el and use it as backend
+      (if (require 'ox-gfm nil t)
+          (setq backend 'gfm)
+        (require 'ox-md))
+      (setq blog (org-export-as backend t))
+      ;; Normalize save file path
+      (unless (string-match "^[/~]" file)
+        (setq file (concat hugo-content-dir file))
+      (unless (string-match "\\.md$" file)
+        (setq file (concat file ".md")))
+      (message "%s" file)
+      ;; save markdown
+      (with-temp-buffer
+        (insert fm)
+        (insert blog)
+        (untabify (point-min) (point-max))
+        (write-file file)
+        (message "Exported to %s" file))
+      ))))
+(bind-key "M-g h" #'hugo)
+
+(defun gp-org-gfm-publish-to-md (plist filename pub-dir)
+  "Publish an org file to Github Flavoured Markdown.
+
+FILENAME is the filename of the Org file to be published.  PLIST
+is the property list for the given project.  PUB-DIR is the
+publishing directory.
+
+Return output file name."
+  (org-publish-org-to 'gfm filename ".md" plist pub-dir))
+
+(setq org-publish-project-alist
+      `(
+        ("12345"
+         ;; Path to org files
+         :base-directory ,(expand-file-name (concat hugo-content-dir "../org-content"))
+         :base-extension "org"
+
+         ;; Path to hugo project
+         :publishing-directory ,hugo-content-dir
+         :recursive t
+         :publishing-function gp-org-gfm-publish-to-md
+         )
+
+        ))
+
+
+
 (defun hrs/de-unicode ()
   "Tidy up a buffer by replacing all special Unicode characters
        (smart quotes, etc.) with their more sane cousins"
